4248ccc8dfee7c8186afcf32611d28da70ab88dc
martinzapletal <zapletal-martin@email.cz>
2014-07-03 23:11:28 +0100

    Method to get letter from number on phone

--- src/main/scala/telephoneNumber/TelephoneNumber.scala
+++ src/main/scala/telephoneNumber/TelephoneNumber.scala
@@ -1,7 +1,12 @@
 package telephoneNumber
 
 class TelephoneNumber {
-  def allCombinationsOfWords(phoneNumber: List[Int]): List[String] = {
+  def getLetterAtPosition(number: Int, position: Int): Char = {
+    assert(number >= 1 && number <= 9 && position >= 1 && position <= 3)
+    (61 + number * 3 + position).toChar
+  }
 
+  def allCombinationsOfWords(phoneNumber: List[Int]): List[String] = {
+    List()
   }
 }
