5eaff262a0adce1f7796fe09171a63bd44a1c0a0
martinzapletal <zapletal-martin@email.cz>
2014-07-04 00:09:07 +0100

    More comprehensive test + fixed algorithm

--- src/main/scala/telephoneNumber/TelephoneNumber.scala
+++ src/main/scala/telephoneNumber/TelephoneNumber.scala
@@ -2,18 +2,18 @@ package telephoneNumber
 
 class TelephoneNumber {
   def getLetterAtPosition(number: Int, position: Int): String = {
-    assert(number >= 1 && number <= 9 && position >= 1 && position <= 3)
-    (61 + number * 3 + position).toChar.toString
+    assert(number >= 1 && number <= 9 && position >= 0 && position <= 2)
+    (62 + number * 3 + position).toChar.toString
   }
 
   def allCombinationsOfWords(phoneNumber: List[Int]): List[String] = {
     def allCombinationsOfWords(digit: Int): List[String] = {
       (0 to 2).flatMap(i =>
-        if(digit == phoneNumber.length) {
-          List()
-        } else {
+        if(digit == phoneNumber.length - 1)
+          List(getLetterAtPosition(phoneNumber(digit), i))
+        else
           allCombinationsOfWords(digit + 1).map(getLetterAtPosition(phoneNumber(digit), i) + _)
-        }).toList
+        ).toList
     }
 
     allCombinationsOfWords(0)
--- src/test/scala/telephoneNumber/TelephoneNumberTest.scala
+++ src/test/scala/telephoneNumber/TelephoneNumberTest.scala
@@ -4,22 +4,42 @@ import org.specs2.mutable.Specification
 
 class TelephoneNumberTest extends Specification {
   "Telephone number" should {
-    lazy val result = new TelephoneNumber().allCombinationsOfWords(List(1,2))
+    lazy val result = new TelephoneNumber().allCombinationsOfWords(List(1,2,3))
 
     "Return all 9 possible combinations" in {
-      result must have size(9)
+      result must have size(27)
     }
 
     "Return all combinations of words created from telephone number as on phone keyboard (2=>ABC, 3=>DEF, ..)" in {
-      result must contain("AD")
-      result must contain("AE")
-      result must contain("AF")
-      result must contain("BD")
-      result must contain("BE")
-      result must contain("BF")
-      result must contain("CD")
-      result must contain("CE")
-      result must contain("CF")
+      result must contain("ADG")
+      result must contain("ADH")
+      result must contain("ADI")
+      result must contain("AEG")
+      result must contain("AEH")
+      result must contain("AEI")
+      result must contain("AFG")
+      result must contain("AFH")
+      result must contain("AFI")
+
+      result must contain("BDG")
+      result must contain("BDH")
+      result must contain("BDI")
+      result must contain("BEG")
+      result must contain("BEH")
+      result must contain("BEI")
+      result must contain("BFG")
+      result must contain("BFH")
+      result must contain("BFI")
+
+      result must contain("CDG")
+      result must contain("CDH")
+      result must contain("CDI")
+      result must contain("CEG")
+      result must contain("CEH")
+      result must contain("CEI")
+      result must contain("CFG")
+      result must contain("CFH")
+      result must contain("CFI")
     }
   }
 }
