30120f0043a1a3c144bbabf770edee671ffc105b
martinzapletal <zapletal-martin@email.cz>
2014-07-09 20:04:50 +0200

    A few more tests

--- src/test/scala/quickSort/QuickSortTest.scala
+++ src/test/scala/quickSort/QuickSortTest.scala
@@ -4,10 +4,18 @@ import org.specs2.mutable.Specification
 
 class QuickSortTest extends Specification {
   "Qicksort" should {
-    lazy val result = new QuickSort().sort(List(3,4,2,1,8,6))
+    val underTest = new QuickSort();
 
-    s"Sort an array of integers" in {
-      result must equalTo(List(1,2,3,4,6,8))
+    "Sort a random list of integers" in {
+      underTest sort(List(3,4,2,1,8,6)) must equalTo(List(1,2,3,4,6,8))
+    }
+
+    "Sort an already sorted list of integers" in {
+      underTest sort(List(1,2,3,4,5,6,7,8,9,10,11)) must equalTo(List(1,2,3,4,5,6,7,8,9,10,11))
+    }
+
+    "Sort a backwards sorted list of integers" in {
+      underTest sort(List(10,9,8,7,6,5,4,3,2,1)) must equalTo(List(1,2,3,4,5,6,7,8,9,10))
     }
   }
 }
