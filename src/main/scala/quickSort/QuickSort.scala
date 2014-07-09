71d57005ea989141d4e27f9a44d6129692c3a8d7
martinzapletal <zapletal-martin@email.cz>
2014-07-09 19:43:52 +0200

    Quicksort setup and test

---
+++ src/main/scala/quickSort/QuickSort.scala
@@ -0,0 +1,13 @@
+package quickSort
+
+class QuickSort {
+  def sort(data: Array[Int]) = {
+    data match {
+      case Nil => Nil
+      case x :: xs => {
+        val lesser = xs.filter(y => true)
+
+      }
+    }
+  }
+}
---
+++ src/test/scala/quickSort/QuickSortTest.scala
@@ -0,0 +1,13 @@
+package quickSort
+
+import org.specs2.mutable.Specification
+
+class QuickSortTest extends Specification {
+  "Qicksort" should {
+    lazy val result = new QuickSort().sort(Array(3,4,2,1,8,6))
+
+    s"Sort an array of integers" in {
+      result must equalTo(Array(1,2,3,4,6,8))
+    }
+  }
+}
