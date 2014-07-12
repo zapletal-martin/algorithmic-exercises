79674791d90388e418e529709c571767f22020a5
martinzapletal <zapletal-martin@email.cz>
2014-07-12 23:08:01 +0200

    Combinations of a string

--- src/main/scala/combinationsOfAString/CombinationsOfAString.scala
+++ src/main/scala/combinationsOfAString/CombinationsOfAString.scala
@@ -1,23 +1,5 @@
 package combinationsOfAString
 
 class CombinationsOfAString {
-  def combine(input: String): List[String] = combine(input.toCharArray.toList)
-
-  def combine(input: List[Char]): List[String] = {
-    input match {
-      case list @ x :: xs => all("", list) ::: combine(xs)
-      case one @ x :: Nil => all("", one)
-    }
-  }
-
-  def all(prefix: String, input: List[Char]): List[String] = {
-    (prefix, input) match {
-      case (prefix, x :: Nil) => prefix + x.toString :: Nil
-      case (prefix, list @ x :: xs) => prefix + x.toString :: all(prefix + x.toString, list)
-    }
-  }
-
-  def cycle(prefix: String, input: List[Char], index: Int): List[String] =
-    prefix + input(index) :: cycle(prefix, input, index + 1)
-
+  def combine(input: String): List[String] = ???
 }
