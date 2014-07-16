d852706e9916d92ab2cc7660251e4e81cba0e3b4
martinzapletal <zapletal-martin@email.cz>
2014-07-17 00:10:39 +0100

    Implementation of lists to lists to list

--- src/main/scala/listOfListsToList/ListOfListsToList.scala
+++ src/main/scala/listOfListsToList/ListOfListsToList.scala
@@ -2,6 +2,9 @@ package listOfListsToList
 
 class ListOfListsToList {
   def unwind[A](input: List[List[A]]): List[A] = {
-    
+    input match {
+      case Nil => Nil
+      case x :: xs => x ::: unwind(xs)
+    }
   }
 }
